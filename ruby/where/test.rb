977fcb997cc141bb2c586f655962eaf4b0ba8d64
Nickolay Schwarz <tjbladez@gmail.com>
2015-05-13 10:23:03 -0500

    Adding the 'typo' back

--- ruby/where/test.rb
+++ ruby/where/test.rb
@@ -11,7 +11,7 @@ class WhereTest < Minitest::Test
   end
 
   def test_where_with_exact_match
-    assert_equal [@wolf], @fixtures.where(:name => 'The Wolf')
+    assert_equal [@wolf], @fixtures.where(:name => 'The Wolf'),
   end
 
   def test_where_with_partial_match
